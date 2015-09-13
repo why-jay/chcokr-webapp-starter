4602a32ee589997e716a31d90cd0d8f1f0206d7c
YJ Yang <chcokr@gmail.com>
2015-09-13 15:36:22 -0400

    Fix lint error

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import App from './App';
 
 import React from 'react';
 
-var containerElem = document.getElementById('react_app_container');
+let containerElem = document.getElementById('react_app_container');
 if (!containerElem) {
   containerElem = document.createElement('div');
   containerElem.id = 'react_app_container';
