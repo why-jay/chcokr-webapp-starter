3c7e4b4cde98e282a0f8db89548d43aef40f6c3c
YJ Yang <chcokr@gmail.com>
2015-11-03 23:26:00 -0800

    entry_point: leverage hjs-webpack@5.0's new #root feature

--- src/entry_point_do_not_touch.jsx
+++ src/entry_point_do_not_touch.jsx
@@ -9,10 +9,5 @@ import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-let containerElem = document.getElementById('react_app_container');
-if (!containerElem) {
-  containerElem = document.createElement('div');
-  containerElem.id = 'react_app_container';
-  document.body.appendChild(containerElem);
-}
-ReactDOM.render(<App />, containerElem);
+// #root is a <div> produced by hjs-webpack's default HTML template.
+ReactDOM.render(<App />, document.getElementById('root'));
